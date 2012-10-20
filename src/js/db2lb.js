4dea851a751e5be4c75c06e4c2ec5b9d3aab0a4e
Huayi Zhang <irachex@gmail.com>
2012-10-20 21:59:42 +0800

    bugfix

--- src/js/db2lb.js
+++ src/js/db2lb.js
@@ -2,7 +2,7 @@ var base_search_url = "http://202.120.227.11/F/?func=find-b&find_base=FDU01&find
 var base_detail_url = "";
 
 function getLibraryButton(keyword) {
-    var search_url = base_search_url.replace("{{data}}", keyword);
+    var search_url = base_search_url.replace("{{data}}", encodeURIComponent(keyword));
     var borrowButton = $('<a href="'+search_url+'" style="float:left;display: inline-block;background: #33A057;border: 1px solid #2F7B4B;color: white;padding: 1px 10px;border-radius:3px;margin-right: 8px;" target="_blank">借阅</a>');
     return borrowButton;
 }
@@ -13,7 +13,7 @@ var url = window.location.toString();
 if ( url.indexOf('subject')!=-1 ){
     var keyword;
     if ($("#info span:last").html().indexOf("ISBN")!=-1) {
-        keyword = $("#info").contents().slice(-2,-1)[0].nodeValue.trim();
+        keyword = $("#info").contents().slice(-3,-2)[0].nodeValue.trim();
     }
     else {
         keyword = $("#mainpic img").attr("alt");
